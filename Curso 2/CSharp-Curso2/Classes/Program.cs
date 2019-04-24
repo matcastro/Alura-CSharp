1a047a785345fbb4990bb32b732177bb635c1fd2
Mateus Castro <matcastro>
2019-04-23 22:49:59 -0300

    Composição de classes e referências.

--- Curso 2/CSharp-Curso2/Classes/Program.cs
+++ Curso 2/CSharp-Curso2/Classes/Program.cs
@@ -11,34 +11,22 @@ namespace Classes
         static void Main(string[] args)
         {
             ContaCorrente contaMateus = new ContaCorrente();
+            contaMateus.titular = new Cliente();
+            contaMateus.titular.nome = "Nome Teste";
+            contaMateus.titular.cpf = "12345678910";
+            contaMateus.titular.profissao = "Pedreiro";
+            contaMateus.numero = 1;
+            contaMateus.Depositar(10);
+            contaMateus.agencia = 1;
+
+            Console.WriteLine(contaMateus.titular);
+            Console.WriteLine(contaMateus.titular.nome);
+            Console.WriteLine(contaMateus.titular.cpf);
+            Console.WriteLine(contaMateus.titular.profissao);
+            Console.WriteLine(contaMateus.agencia);
+            Console.WriteLine(contaMateus.numero);
             Console.WriteLine(contaMateus.saldo);
 
-            contaMateus.Depositar(200);
-            Console.WriteLine(contaMateus.saldo);
-
-            contaMateus.Sacar(50);
-            Console.WriteLine(contaMateus.saldo);
-
-            ContaCorrente contaTati = new ContaCorrente();
-            Console.WriteLine(contaTati.saldo);
-            contaMateus.Transferir(100, contaTati);
-            Console.WriteLine(contaMateus.saldo);
-            Console.WriteLine(contaTati.saldo);
-
-            contaMateus.Depositar(-200);
-            Console.WriteLine(contaMateus.saldo);
-
-            contaMateus.Sacar(5000);
-            Console.WriteLine(contaMateus.saldo);
-
-            contaMateus.Sacar(-50);
-            Console.WriteLine(contaMateus.saldo);
-
-            contaMateus.Transferir(1000, contaTati);
-            Console.WriteLine(contaMateus.saldo);
-
-            contaMateus.Transferir(-100, contaTati);
-            Console.WriteLine(contaMateus.saldo);
             Console.ReadLine();
         }
     }
