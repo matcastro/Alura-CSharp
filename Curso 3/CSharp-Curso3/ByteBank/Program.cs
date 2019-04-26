245c1029f163e5c12e9158d6a623dc18a8898896
Mateus Castro <matcastro>
2019-04-25 21:43:49 -0300

    Sobrecarga de métodos.

---
+++ Curso 3/CSharp-Curso3/ByteBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+            Funcionario carlos = new Funcionario();
+            carlos.Nome = "Carlos";
+            carlos.CPF = "123.456.789-10";
+            carlos.Salario = 2000;
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "109.876.543-21";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(carlos);
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine("Total de bonificações: R$ " + gerenciador.GetTotalBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
