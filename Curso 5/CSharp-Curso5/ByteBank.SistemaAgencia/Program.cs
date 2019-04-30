127c7097b3fa30e524aa7bb20ea205df8f5d57e4
Mateus Castro <matcastro>
2019-04-29 23:15:14 -0300

    Importação dos arquivos e criação da primeira DLL

---
+++ Curso 5/CSharp-Curso5/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(2156, 348646);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
