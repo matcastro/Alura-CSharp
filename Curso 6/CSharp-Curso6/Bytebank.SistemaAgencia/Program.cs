2a5a6d146bf759e7dc6b1aa770d74ef8415358d3
Mateus Castro <matcastro>
2019-05-04 16:13:09 -0300

    Conhecendo melhor a classe string com os métodos Substring e IndexOf para pegar os argumentos de uma URL.

---
+++ Curso 6/CSharp-Curso6/Bytebank.SistemaAgencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "pagina?argumentos";
+            Console.WriteLine(url);
+            url = url.Substring(url.IndexOf("?") + 1);
+            Console.WriteLine(url);
+            Console.ReadLine();
+        }
+    }
+}
