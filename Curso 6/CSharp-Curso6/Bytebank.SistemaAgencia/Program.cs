ff847b7748a72c228cc803dc2b985465cb4b8f47
Mateus Castro <matcastro>
2019-05-05 17:50:19 -0300

    Utilizando regex para buscar padrão de telefone.

--- Curso 6/CSharp-Curso6/Bytebank.SistemaAgencia/Program.cs
+++ Curso 6/CSharp-Curso6/Bytebank.SistemaAgencia/Program.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Bytebank.SistemaAgencia
 {
     class Program
     {
+        readonly static string padraoTelefone = "[0-9]{4,5}[- ]?[0-9]{4}";
         static void Main(string[] args)
         {
+            Console.WriteLine(RecuperarTelefoneDeTexto("Meu número é: 23423453"));
+            Console.ReadLine();
+
             string url = "pagina?moedaOrigem=real&moedaDestino=dolar&valor=1500";
 
             ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(url);
@@ -21,6 +26,12 @@ namespace Bytebank.SistemaAgencia
             Console.WriteLine(valor);
 
             Console.ReadLine();
+        }
+
+        static string RecuperarTelefoneDeTexto(string texto)
+        {
+            Match telefone = Regex.Match(texto, padraoTelefone);
+            return telefone.Value;
         }
     }
 }
