6efcfdfe0378800dce49dfcd95d0fdd4e1aa7c06
Mateus Castro <matcastro>
2019-05-07 23:12:49 -0300

    Aula sobre arrays

---
+++ Curso 7/CSharp-Curso7/ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] idades = new int[] { 15, 28, 35, 50, 28 };
+
+            int acumulador = 0;
+
+            for (int indice = 0; indice < idades.Length; indice++)
+            {
+                acumulador += idades[indice];
+                Console.WriteLine($"Valor no indice {indice} {idades[indice]}");
+            }
+
+            int media = acumulador / idades.Length;
+            Console.WriteLine(acumulador);
+            Console.WriteLine(media);
+            Console.ReadLine();
+        }
+    }
+}
