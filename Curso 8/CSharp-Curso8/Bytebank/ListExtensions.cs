3bad2fa0d0eb6905557f88694ba75db8d4bb5322
Mateus Castro <matcastro>
2019-05-15 21:55:25 -0300

    Utilização do tipo List do .NET e criação de método de extensão para adicionar vários elementos de uma vez.

---
+++ Curso 8/CSharp-Curso8/Bytebank/ListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank
+{
+    public static class ListExtensions
+    {
+        public static void AdicionarVarios(this List<int> lista, params int[] itens)
+        {
+            foreach (int item in itens)
+            {
+                lista.Add(item);
+            }
+        }
+    }
+}
---
+++ Curso 8/CSharp-Curso8/Bytebank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> idades = new List<int>();
+
+            idades.AdicionarVarios(16, 63, 52, 94);
+            idades.Remove(63);
+
+            for (int i = 0; i < idades.Count; i++)
+            {
+                Console.WriteLine(idades[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
