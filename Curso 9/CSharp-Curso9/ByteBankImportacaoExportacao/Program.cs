6eee9b5bef52c3c367810185266dfbba3f3e5eb0
Mateus Castro <matcastro>
2019-05-18 13:01:51 -0300

    Leitura de arquivo, com criação do buffer e usando o FileStream.

---
+++ Curso 9/CSharp-Curso9/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,36 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            var arquivo = "contas.txt";
+            var fluxoDoArquivo = new FileStream(arquivo, FileMode.Open);
+            var buffer = new byte[1024];
+            var bytesLidos = -1;
+
+            while(bytesLidos != 0)
+            {
+                bytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+            Console.ReadLine();
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            var enconding = Encoding.UTF8;
+            var texto = enconding.GetString(buffer);
+            Console.Write(texto);
+        }
+    }
+} 
+ 
